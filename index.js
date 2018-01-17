f66e9358e74a2ef3d9298218406b204764524bf9
Daniel Dunbar <danielqdunbar@gmail.com>
2018-01-17 01:48:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var theBeatlesPlay = (musicians, instruments) => {
+  var stringArray = [];
+  for (var n=0; n<musicians.length; n++) {
+    stringArray[n] = `${musicians[n]} plays ${instruments[n]}`
+  }
+  return stringArray
+}
+
+var johnLennonFacts = (factArray) => {
+  var excitedFacts = []
+  let counter = 0;
+  while (counter < factArray.length) {
+    excitedFacts[counter] = `${factArray[counter]}!!!`
+    counter--
+  }
+  return excitedFacts
+}
